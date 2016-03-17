b68255f0cde0eaa9141b5dfe31ab287c244229f1
anna hunter <annfmonty@gmail.com>
2016-03-16 22:49:25 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//Constructor for Card
+console.log('hello');
+
+
+/*var card = MakeCard();
+function MakeCard() {
+	this.suit = "Ace";
+	this.num = "5";
+}
+Console.log();*/
---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+//Constructor for Card
+
+function MakeCard() {
+	this.suit = "Ace";
+	this.num = "5";
+}
+
+var card =  new MakeCard();
+console.log(card);
+
+//Constructor for a die.
+
+function MakeDie(){
+	this.side1 = 1;
+	this.side2 = 2;
+	this.side3 = 3;
+	this.side4 = 4;
+	this.side5 = 5;
+	this.side6 = 6;
+	this.roll = function(){
+		return Math.ceil(Math.random() * 6);
+	}
+};
+var die = new MakeDie();
+
+//Rolling the dice.
+
+var dieRoll = die.roll(); // return a number between 1 and 6
+console.log("You rolled a " +dieRoll);
+
+//Probabilities
+
+var result = function getProbabilities(){
+	for(var i = 0; i < 1000; i++){
+	var roll1 = die.roll();
+	var roll2 = die.roll();
+	var sumroll = Array.of(roll1 + roll2);
+	console.log(sumroll);
+	}
+};
+
+
+console.log(result());
+
+
+
+
+
+
+
+
+
